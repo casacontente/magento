4c2e0b4a89b7a849c9d1fd76411617c75a568082
elenarantza <elenarantza@gmail.com>
2016-07-18 11:05:04 -0300

    banner slider

---
+++ app/code/community/CapacityWebSolutions/Responsivebannerslider/Model/Config/Source/Animationdirection.php
@@ -0,0 +1,17 @@
+<?php
+/***************************************************************************
+ Extension Name  : Magento Responsive Banner Slider with Lazy Load Extension
+ Extension URL   : http://www.magebees.com/magento-responsive-banner-slider-with-lazy-load-extension.html
+ Copyright    : Copyright (c) 2016 MageBees, http://www.magebees.com
+ Support Email   : support@magebees.com 
+ ***************************************************************************/
+
+class CapacityWebSolutions_Responsivebannerslider_Model_Config_Source_Animationdirection
+{
+    public function toOptionArray(){
+        return array(
+            array('value' => 'horizontal', 'label'=>Mage::helper('adminhtml')->__('Horizontal')),
+            array('value' => 'vertical', 'label'=>Mage::helper('adminhtml')->__('Vertical')),
+        );
+    }
+}
